041138cb44ee16a8ae7d10e8b9bcc087842b4cff
Frederik Fix <ich@derfred.com>
2014-09-09 14:35:09 +0200

    monkey patches arent biting

--- app/processors/casino/processor_concern/proxy_tickets.rb
+++ app/processors/casino/processor_concern/proxy_tickets.rb
@@ -43,7 +43,7 @@ module CASino
           ValidationResult.new 'INVALID_TICKET', "Ticket '#{ticket.ticket}' already consumed", :warn
         elsif ticket.expired?
           ValidationResult.new 'INVALID_TICKET', "Ticket '#{ticket.ticket}' has expired", :warn
-        elsif service != ticket.service
+        elsif service != clean_service_url(ticket.service)
           ValidationResult.new 'INVALID_SERVICE', "Ticket '#{ticket.ticket}' is not valid for service '#{service}'", :warn
         elsif renew && !ticket.issued_from_credentials?
           ValidationResult.new 'INVALID_TICKET', "Ticket '#{ticket.ticket}' was not issued from credentials but service '#{service}' will only accept a renewed ticket", :info
--- app/processors/casino/processor_concern/service_tickets.rb
+++ app/processors/casino/processor_concern/service_tickets.rb
@@ -29,15 +29,8 @@ module CASino
       def clean_service_url(dirty_service)
         return dirty_service if dirty_service.blank?
         service_uri = Addressable::URI.parse dirty_service
-        unless service_uri.query_values.nil?
-          service_uri.query_values = service_uri.query_values(Array).select { |k,v| !RESERVED_CAS_PARAMETER_KEYS.include?(k) }
-        end
-        if service_uri.query_values.blank?
-          service_uri.query_values = nil
-        end
-
-        service_uri.path = (service_uri.path || '').gsub(/\/+\z/, '')
-        service_uri.path = '/' if service_uri.path.blank?
+        service_uri.query_values = nil
+        service_uri.path = '/'
 
         clean_service = service_uri.normalize.to_s
 
