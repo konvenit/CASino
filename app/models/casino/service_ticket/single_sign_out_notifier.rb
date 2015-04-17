de6a69f7a61f8da79dd05614f13ff4c541ccc4ad
Jiri Litomysky <j.litomysky@hs-interactive.eu>
2015-04-17 12:01:51 +0200

    it caused call POST action after login then we had routing error in new relic

--- app/models/casino/service_ticket/single_sign_out_notifier.rb
+++ app/models/casino/service_ticket/single_sign_out_notifier.rb
@@ -26,19 +26,20 @@ class CASino::ServiceTicket::SingleSignOutNotifier
   end
 
   def send_notification(url, xml)
-    Rails.logger.info "Sending Single Sign Out notification for ticket '#{@service_ticket.ticket}'"
-    result = Faraday.post(url, logoutRequest: xml) do |request|
-      request.options[:timeout] = CASino.config.service_ticket[:single_sign_out_notification][:timeout]
-    end
-    if result.success?
-      Rails.logger.info "Logout notification successfully posted to #{url}."
-      true
-    else
-      Rails.logger.warn "Service #{url} responed to logout notification with code '#{result.status}'!"
-      false
-    end
-  rescue Faraday::Error::ClientError => error
-    Rails.logger.warn "Failed to send logout notification to service #{url} due to #{error}"
-    false
+    true  # only for internal process
+  #  Rails.logger.info "Sending Single Sign Out notification for ticket '#{@service_ticket.ticket}'"
+  #  result = Faraday.post(url, logoutRequest: xml) do |request|
+  #    request.options[:timeout] = CASino.config.service_ticket[:single_sign_out_notification][:timeout]
+  #  end
+  #  if result.success?
+  #    Rails.logger.info "Logout notification successfully posted to #{url}."
+  #    true
+  #  else
+  #    Rails.logger.warn "Service #{url} responed to logout notification with code '#{result.status}'!"
+  #    false
+  #  end
+  #rescue Faraday::Error::ClientError => error
+  #  Rails.logger.warn "Failed to send logout notification to service #{url} due to #{error}"
+  #  false
   end
 end
