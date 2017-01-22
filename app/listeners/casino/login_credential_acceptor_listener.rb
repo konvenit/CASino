66dbeaefe495dc56af9f9af4abbb80a8e84b97e0
Alexander Nikolskiy <a.nikolskiy.developer@hs-interactive.eu>
2017-01-22 19:18:41 +0100

    http only tgt cookie

--- app/listeners/casino/login_credential_acceptor_listener.rb
+++ app/listeners/casino/login_credential_acceptor_listener.rb
@@ -4,7 +4,7 @@ class CASino::LoginCredentialAcceptorListener < CASino::Listener
   attr_reader :controller
 
   def user_logged_in(url, ticket_granting_ticket, cookie_expiry_time = nil)
-    @controller.cookies[:tgt] = { value: ticket_granting_ticket, expires: cookie_expiry_time }
+    @controller.cookies[:tgt] = { value: ticket_granting_ticket, expires: cookie_expiry_time, httponly: true }
     if url.nil?
       @controller.redirect_to sessions_path, status: :see_other
     else
